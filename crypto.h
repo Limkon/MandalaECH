d4c9913432738b11a86c965c0e678d437ec4da18
Limkon <limkon@skvip.eu.org>
2025-12-13 22:20:54 +0800

    Update crypto.h

--- crypto.h
+++ crypto.h
@@ -1,25 +1,31 @@
 #ifndef CRYPTO_H
 #define CRYPTO_H
 
+#include <winsock2.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
 #include "common.h"
 
-// OpenSSL 初始化與清理
+typedef struct {
+    SOCKET sock;
+    SSL *ssl;
+} TLSContext;
+
+extern SSL_CTX *g_ssl_ctx;
+
 void init_openssl_global();
 void FreeGlobalSSLContext();
 
-// TLS 連接操作
-int tls_init_connect(TLSContext *ctx);
+// [修改] 增加 mode 参数: 0=默认(温和), 1=强力(自动重试时使用)
+int tls_init_connect(TLSContext *ctx, int mode);
+
 int tls_write(TLSContext *ctx, const char *data, int len);
 int tls_read(TLSContext *ctx, char *out, int max);
 int tls_read_exact(TLSContext *ctx, char *buf, int len);
 void tls_close(TLSContext *ctx);
 
-// WebSocket 封裝
 int build_ws_frame(const char *in, int len, char *out);
 int check_ws_frame(unsigned char *in, int len, int *head_len, int *payload_len);
 int ws_read_payload_exact(TLSContext *tls, char *out_buf, int expected_len);
 
-// BIO 分片過濾器 (內部使用，但若需要可在這裡聲明)
-BIO_METHOD *BIO_f_fragment(void);
-
-#endif // CRYPTO_H
+#endif
