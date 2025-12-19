9755eda61eb3d1485d4ddca61cf0562087657568
Limkon <limkon@skvip.eu.org>
2025-12-19 16:38:12 +0800

    Update utils.h

--- utils.h
+++ utils.h
@@ -3,23 +3,32 @@
 
 #include "common.h"
 
+// 日志记录函数
 void log_msg(const char *format, ...);
+// 记录 Winsock 错误
 void log_wsa_error(const char* context);
-char* GetClipboardText(); 
-void TrimString(char* str);
-void UrlDecode(char* dst, const char* src);
-unsigned char* Base64Decode(const char* src, size_t* out_len);
-char* GetQueryParam(const char* query, const char* key);
+
+// 文件读写辅助
 BOOL ReadFileToBuffer(const wchar_t* filename, char** buffer, long* fileSize);
 BOOL WriteBufferToFile(const wchar_t* filename, const char* buffer);
 
-// --- 网络功能 (新增) ---
-// 下载指定 URL 内容，超时时间固定为 10 秒
+// 字符串辅助
+void TrimString(char* str);
+void UrlDecode(char* dst, const char* src);
+char* GetQueryParam(const char* query, const char* key);
+unsigned char* Base64Decode(const char* src, size_t* out_len);
+char* GetClipboardText();
+
+// HTTP 请求 (文本版)
 char* Utils_HttpGet(const char* url);
 
-// --- 系统代理功能 ---
-BOOL IsWindows8OrGreater();
-void SetSystemProxy(BOOL enable);
-BOOL IsSystemProxyEnabled();
+// [ECH] HTTP 请求 (二进制版)
+char* Utils_HttpBytesGet(const char* url, int* out_len);
 
-#endif // UTILS_H
+// [ECH] Base64Url 编码
+void Base64UrlEncode(const unsigned char* src, int len, char* dst);
+
+// [ECH] 从 DoH 获取 ECH Config (返回 Base64 字符串)
+char* FetchECHFromDoH(const char* dohUrl, const char* sni);
+
+#endif
