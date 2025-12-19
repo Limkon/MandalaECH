f16c642de515288f91b82d04767e8cceeb11028b
Limkon <limkon@skvip.eu.org>
2025-12-19 21:07:46 +0800

    Update common.h

--- common.h
+++ common.h
@@ -4,7 +4,10 @@
 #include <winsock2.h>
 #include <windows.h>
 
-// 定義配置文件的路徑變數
+// 核心常數
+#define BUFFER_SIZE 65536
+
+// 定義配置文件的路徑變量
 extern wchar_t g_iniFilePath[MAX_PATH];
 #define CONFIG_FILE g_iniFilePath
 
@@ -12,7 +15,7 @@ extern wchar_t g_iniFilePath[MAX_PATH];
 typedef struct {
     char type[32];      // 協議類型: vless, trojan, socks, shadowsocks, mandala, vmess
     char tag[256];      // 備註
-    char host[256];     // 伺服器地址 (真實目標 IP 或域名 / Inner SNI)
+    char host[256];     // 服務器地址 (真實目標 IP 或域名 / Inner SNI)
     int port;           // 端口
     
     char user[256];     // UUID 或 用戶名
@@ -21,9 +24,8 @@ typedef struct {
     char sni[256];      // TLS SNI (通常用於普通 TLS，或作為 ECH 的 Outer SNI)
     char path[256];     // WS Path / gRPC ServiceName
     
-    // [ECH 新增字段]
-    char outer_sni[256]; // ECH 偽裝域名 (Outer SNI, 例如 cloudflare-ech.com)
-    char ech[2048];      // ECH 配置字串 (Base64)
+    char outer_sni[256]; // ECH 偽裝域名 (Outer SNI)
+    char ech[2048];      // ECH 配置字符串 (Base64)
 } ProxyConfig;
 
 // 全局變量聲明
@@ -34,17 +36,24 @@ extern BOOL g_proxyRunning;
 extern HANDLE hProxyThread;
 extern SOCKET g_listen_sock;
 
-// 窗口句柄
+// 窗口與菜單句柄
 extern HWND hwnd;
 extern HWND hLogViewerWnd;
+extern HMENU hMenu;
+extern HMENU hNodeSubMenu;
 extern NOTIFYICONDATAW nid;
 extern BOOL g_isIconVisible;
+
+// 窗口回調與滾動狀態
+extern WNDPROC g_oldListBoxProc;
+extern int g_nEditScrollPos;
+extern int g_nEditContentHeight;
 
 // 字體
 extern HFONT hAppFont;
 extern HFONT hLogFont;
 
-// 節點列表
+// 節點管理
 extern wchar_t** nodeTags;
 extern int nodeCount;
 extern wchar_t currentNode[64];
@@ -84,10 +93,7 @@ extern char g_userAgentStr[512];
 #define WM_LOG_UPDATE           (WM_USER + 2)
 #define WM_REFRESH_NODELIST     (WM_USER + 3)
 
-// 顏色
-#define COLOR_BTNFACE           15
-
-// [修正] 改為 extern 聲明，由 globals.c 提供實際定義
+// UA 模板
 extern const char* UA_TEMPLATES[];
 extern const wchar_t* UA_PLATFORMS[];
 
