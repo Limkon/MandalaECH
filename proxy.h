5de32c3c7787e44c8fd96e3fc51045a1e4b7c28a
Limkon <limkon@skvip.eu.org>
2025-12-19 12:53:47 +0800

    Update proxy.h

--- proxy.h
+++ proxy.h
@@ -3,12 +3,18 @@
 
 #include "common.h"
 
-void StartProxyCore();
-void StopProxyCore();
-int recv_timeout(SOCKET s, char *buf, int len, int timeout_sec);
-int send_all(SOCKET s, const char *buf, int len);
+// --- 代理核心控制函数 ---
 
+// 启动代理监听线程
+void StartProxyCore();
+
+// 停止代理并释放资源
+void StopProxyCore();
+
+// 监听线程入口 (通常由 StartProxyCore 内部调用，但如果需要在外部创建线程也可暴露)
 DWORD WINAPI server_thread(LPVOID p);
+
+// 客户端处理线程入口 (通常由 accept 循环调用)
 DWORD WINAPI client_handler(LPVOID p);
 
 #endif // PROXY_H
