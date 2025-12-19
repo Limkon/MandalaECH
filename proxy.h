6508bfb5895009e7d20e4187150445ab58240c28
Limkon <limkon@skvip.eu.org>
2025-12-19 16:46:06 +0800

    Update proxy.h

--- proxy.h
+++ proxy.h
@@ -11,10 +11,10 @@ void StartProxyCore();
 // 停止代理并释放资源
 void StopProxyCore();
 
-// 监听线程入口 (通常由 StartProxyCore 内部调用，但如果需要在外部创建线程也可暴露)
+// 监听线程入口
 DWORD WINAPI server_thread(LPVOID p);
 
-// 客户端处理线程入口 (通常由 accept 循环调用)
+// 客户端处理线程入口
 DWORD WINAPI client_handler(LPVOID p);
 
 #endif // PROXY_H
