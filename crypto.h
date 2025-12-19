36f5b74bbef88dcc0321d50d3ec13b5fce7a713b
Limkon <limkon@skvip.eu.org>
2025-12-19 12:48:34 +0800

    Refactor crypto.h to add ECH parameter and struct definition

--- crypto.h
+++ crypto.h
@@ -3,21 +3,33 @@
 
 #include "common.h"
 
-// 初始化与清理
+// --- 结构体定义 ---
+#ifndef TLS_CONTEXT_DEFINED
+#define TLS_CONTEXT_DEFINED
+typedef struct { 
+    SOCKET sock; 
+    SSL *ssl; 
+} TLSContext;
+#endif
+
+// --- BIO 分片过滤器 ---
+BIO_METHOD *BIO_f_fragment(void);
+
+// --- OpenSSL 全局初始化与销毁 ---
 void init_openssl_global();
 void FreeGlobalSSLContext();
 
-// TLS 连接相关
-// [Fix] 更新为3个参数，以支持线程安全的 SNI 设置
-// 解决了之前 crypto.c 和 crypto.h 签名不匹配导致的编译错误
-int tls_init_connect(TLSContext *ctx, const char* target_sni, const char* target_host);
+// --- TLS 连接函数 (增加 ECH 参数) ---
+// [ECH Refactor] target_sni, target_host, ech_config_b64
+int tls_init_connect(TLSContext *ctx, const char* target_sni, const char* target_host, const char* ech_config_b64);
 
+// --- TLS 读写函数 ---
 int tls_write(TLSContext *ctx, const char *data, int len);
 int tls_read(TLSContext *ctx, char *out, int max);
 int tls_read_exact(TLSContext *ctx, char *buf, int len);
 void tls_close(TLSContext *ctx);
 
-// WebSocket 协议辅助
+// --- WebSocket 辅助函数 ---
 int build_ws_frame(const char *in, int len, char *out);
 long long check_ws_frame(unsigned char *in, int len, int *head_len, int *payload_len);
 int ws_read_payload_exact(TLSContext *tls, char *out_buf, int expected_len);
