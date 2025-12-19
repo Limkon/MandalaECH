e2d76c698df66351271b206ea78883e1215a7efa
Limkon <limkon@skvip.eu.org>
2025-12-19 20:14:15 +0800

    Update resource.h

--- resource.h
+++ resource.h
@@ -1,10 +1,9 @@
 #ifndef RESOURCE_H
 #define RESOURCE_H
 
-// --- 图标资源 ---
 #define IDI_APP_ICON 1
 
-// --- 设置窗口控件 ID (SettingsWnd) ---
+// 設置窗口控件 ID
 #define ID_HOTKEY_CTRL      1101
 #define ID_PORT_EDIT        1102
 #define ID_CHK_CIPHERS      1103
@@ -19,8 +18,7 @@
 #define ID_COMBO_PLATFORM   1112
 #define ID_EDIT_UA_STR      1113
 
-// --- 节点编辑窗口控件 ID (NodeEditWnd) ---
-// 注意：gui.c 中已经定义了 ID_EDIT_ECH_SNI (2050)
+// 節點編輯窗口 ID
 #define ID_EDIT_TAG         2201
 #define ID_EDIT_ADDR        2202
 #define ID_EDIT_PORT        2203
@@ -31,15 +29,16 @@
 #define ID_EDIT_HOST        2208
 #define ID_EDIT_PATH        2209
 #define ID_EDIT_TLS         2210
+#define ID_EDIT_ECH_SNI     2050
 
-// --- 节点管理窗口控件 ID (NodeMgrWnd) ---
-// 注意：gui.c 中已经定义了 ID_NODEMGR_SUB (2003)
+// 節點管理與訂閱
 #define ID_NODEMGR_LIST     3001
 #define ID_NODEMGR_EDIT     3002
 #define ID_NODEMGR_DEL      3003
+#define ID_NODEMGR_SUB      2003
 
-// --- 日志窗口控件 ID (LogWnd) ---
+// 日誌窗口
 #define ID_LOG_CHK          4001
 #define ID_LOGVIEWER_EDIT   4002
 
-#endif // RESOURCE_H
+#endif
