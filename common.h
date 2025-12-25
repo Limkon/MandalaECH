1db8a64b4062d20d0a56bb523e814311d992bea0
Limkon <limkon@skvip.eu.org>
2025-12-25 09:45:42 +0800

    Update common.h

--- common.h
+++ common.h
@@ -17,7 +17,6 @@
 #define _WIN32_WINNT 0x0601
 #endif
 
-// 减少 Windows 头文件包含的内容
 #define WIN32_LEAN_AND_MEAN
 
 // 尝试阻止 windows.h 包含 wincrypt.h
@@ -25,7 +24,7 @@
 
 #define MAX_CONNECTIONS 512
 
-// --- 1. Windows 系统头文件 (必须先包含) ---
+// --- 1. Windows 系统头文件 ---
 #include <windows.h>
 #include <shellapi.h>
 #include <winsock2.h>
@@ -34,39 +33,35 @@
 #include <shlobj.h>
 #include <wininet.h>
 
-// --- 2. [关键修复] 强制清理 Windows 宏污染 ---
-// 即使定义了 NOCRYPT，某些环境仍可能引入这些与 OpenSSL/BoringSSL 冲突的宏。
-// 必须在包含 OpenSSL 头文件之前手动 undef 它们。
+// --- 2. 引入资源头文件 (解决重定义警告) ---
+// [Fix] 必须包含此文件以获取 GUI 控件 ID (ID_CHK_FRAG 等)
+// 确保 common.h 中的 ID 与 resource.rc 使用的 ID 一致
+#include "resource.h"
 
+// --- 3. 清理 Windows 宏污染 ---
 #ifdef X509_NAME
 #undef X509_NAME
 #endif
-
 #ifdef X509_EXTENSIONS
 #undef X509_EXTENSIONS
 #endif
-
 #ifdef X509_CERT_PAIR
 #undef X509_CERT_PAIR
 #endif
-
 #ifdef PKCS7_ISSUER_AND_SERIAL
 #undef PKCS7_ISSUER_AND_SERIAL
 #endif
-
 #ifdef PKCS7_SIGNER_INFO
 #undef PKCS7_SIGNER_INFO
 #endif
-
 #ifdef OCSP_REQUEST
 #undef OCSP_REQUEST
 #endif
-
 #ifdef OCSP_RESPONSE
 #undef OCSP_RESPONSE
 #endif
 
-// --- 3. 标准 C 头文件 ---
+// --- 4. 标准 C 头文件 ---
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -74,7 +69,7 @@
 #include <time.h>
 #include <ctype.h>
 
-// --- 4. OpenSSL/BoringSSL 头文件 (现在是安全的) ---
+// --- 5. OpenSSL/BoringSSL 头文件 ---
 #include <openssl/ssl.h>
 #include <openssl/err.h>
 #include <openssl/bio.h>
@@ -87,9 +82,7 @@
 #define REG_PATH_PROXY L"Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings"
 #define CONFIG_FILE L"config.json"
 
-// IO_BUFFER_SIZE: 用于 TCP 接收和初始 WebSocket 缓冲区 (16KB)
 #define IO_BUFFER_SIZE 16384 
-// MAX_WS_FRAME_SIZE: WebSocket 最大允许帧大小 (8MB)
 #define MAX_WS_FRAME_SIZE 8388608 
 
 // Windows Messages
@@ -97,7 +90,7 @@
 #define WM_LOG_UPDATE (WM_USER + 2)
 #define WM_REFRESH_NODELIST (WM_USER + 50)
 
-// Command IDs
+// Command IDs (这些通常不在 resource.h 中，保留)
 #define ID_TRAY_EXIT 1001
 #define ID_TRAY_AUTORUN 1002
 #define ID_TRAY_SYSTEM_PROXY 1003
@@ -109,44 +102,13 @@
 #define ID_GLOBAL_HOTKEY 9001
 #define ID_TRAY_NODE_BASE 2000
 
-// GUI Control IDs
-#define ID_LOGVIEWER_EDIT 6001
-#define ID_LOG_CHK        6002 
-#define ID_NODEMGR_LIST 3001
-#define ID_NODEMGR_DEL 3002
-#define ID_NODEMGR_EDIT 3003 
-#define ID_HOTKEY_CTRL 7001 
-#define ID_PORT_EDIT 7002
+// [Fix] 下面的 ID 已在 resource.h 中定义，此处移除以消除警告
+// ID_LOGVIEWER_EDIT, ID_LOG_CHK, ID_NODEMGR_... 等已由 #include "resource.h" 提供
 
-// Settings GUI IDs
-#define ID_CHK_CIPHERS    7010
-#define ID_CHK_ALPN       7011
-#define ID_COMBO_PLATFORM 7012 
-#define ID_EDIT_UA_STR    7013 
-#define ID_CHK_FRAG       7014
-#define ID_EDIT_FRAG_DLY  7016 
-#define ID_EDIT_FRAG_MIN  7017
-#define ID_EDIT_FRAG_MAX  7018
-#define ID_CHK_PADDING    7019
-#define ID_EDIT_PAD_MIN   7020
-#define ID_EDIT_PAD_MAX   7021
-
-// ECH 设置控件 ID
+// ECH 设置控件 ID (如果在 resource.h 中未定义，则保留；如果未来报错重定义，请删除)
 #define ID_CHK_ECH         7022
 #define ID_EDIT_ECH_SERVER 7023
 #define ID_EDIT_ECH_DOMAIN 7024
-
-// Node Edit IDs
-#define ID_EDIT_TAG      8001
-#define ID_EDIT_ADDR     8002
-#define ID_EDIT_PORT     8003
-#define ID_EDIT_USER     8004
-#define ID_EDIT_PASS     8005
-#define ID_EDIT_NET      8006
-#define ID_EDIT_TYPE     8007
-#define ID_EDIT_HOST     8008
-#define ID_EDIT_PATH     8009
-#define ID_EDIT_TLS      8010
 
 // --- 结构体定义 ---
 typedef struct { 
