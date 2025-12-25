02acd21e11e3c2fa6259d1a87b008baac4709fbf
Limkon <limkon@skvip.eu.org>
2025-12-25 09:39:05 +0800

    Update utils.h

--- utils.h
+++ utils.h
@@ -3,9 +3,11 @@
 
 #include "common.h"
 
+// --- 基础工具函数 ---
 void log_msg(const char *format, ...);
-void log_wsa_error(const char* context);
-char* GetClipboardText(); 
+// [Delete] log_wsa_error 未在 utils.c 实现，已删除
+
+char* GetClipboardText();
 void TrimString(char* str);
 void UrlDecode(char* dst, const char* src);
 unsigned char* Base64Decode(const char* src, size_t* out_len);
@@ -13,13 +15,16 @@ char* GetQueryParam(const char* query, const char* key);
 BOOL ReadFileToBuffer(const wchar_t* filename, char** buffer, long* fileSize);
 BOOL WriteBufferToFile(const wchar_t* filename, const char* buffer);
 
-// --- 网络功能 (新增) ---
-// 下载指定 URL 内容，超时时间固定为 10 秒
+// --- 网络功能 ---
 char* Utils_HttpGet(const char* url);
 
 // --- 系统代理功能 ---
 BOOL IsWindows8OrGreater();
 void SetSystemProxy(BOOL enable);
 BOOL IsSystemProxyEnabled();
+
+// --- [新增] ECH 功能 ---
+// 供 crypto.c 调用
+unsigned char* FetchECHConfig(const char* domain, const char* doh_server, size_t* out_len);
 
 #endif // UTILS_H
