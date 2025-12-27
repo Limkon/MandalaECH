7fb6bf253f273f7d8a682f14ccee39cc70cda6c0
Limkon <limkon@skvip.eu.org>
2025-12-27 20:57:30 +0800

    Update common.h

--- common.h
+++ common.h
@@ -1,13 +1,19 @@
 #ifndef COMMON_H
 #define COMMON_H
 
+// [Fix] 必须在包含任何 Windows 头文件之前定义，启用 inet_ntop/getaddrinfo 等
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0600
+#endif
+
 #include <winsock2.h>
+#include <ws2tcpip.h> // [Fix] 必须包含此头文件以支持 getaddrinfo, struct addrinfo, inet_ntop
 #include <windows.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <wchar.h>
-#include <shellapi.h> // [Fix] for NOTIFYICONDATAW
+#include <shellapi.h>
 
 // --- 基础配置 ---
 #define CONFIG_FILE L"config.json"
@@ -15,8 +21,8 @@
 #define MAX_TOTAL_MEMORY_USAGE (512 * 1024 * 1024)
 
 // --- 自定义消息与常量 ---
-#define WM_LOG_UPDATE (WM_USER + 2) // [Fix] Added missing definition
-#define REG_PATH_PROXY L"Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings" // [Fix] Added missing definition
+#define WM_LOG_UPDATE (WM_USER + 2) 
+#define REG_PATH_PROXY L"Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings" 
 
 // 订阅结构定义
 typedef struct {
@@ -64,7 +70,7 @@ extern const char* UA_TEMPLATES[];
 extern unsigned int g_hotkeyModifiers;
 extern unsigned int g_hotkeyVk;
 
-// 8. GUI 状态 (供 config.c 使用) [Fix] Expose GUI globals
+// 8. GUI 状态
 extern NOTIFYICONDATAW nid;
 extern BOOL g_isIconVisible;
 
